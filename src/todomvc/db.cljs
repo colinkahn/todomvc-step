51653dc3074b6f0c758ddc3d0cebbf03e1c5abd6
Colin Kahn <info@colinkahn.com>
2018-09-18 14:52:04 -0700

    use pr-str for saving to localStorage

--- src/todomvc/db.cljs
+++ src/todomvc/db.cljs
@@ -132,7 +132,7 @@
 (defn todos->local-store
   "Puts todos into localStorage"
   [todos]
-  (.setItem js/localStorage ls-key (str todos)))     ;; sorted-map written as an EDN map
+  (.setItem js/localStorage ls-key (pr-str todos)))   ;; sorted-map written as an EDN map
 
 
 ;; -- cofx Registrations  -----------------------------------------------------
